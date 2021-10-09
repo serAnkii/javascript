7283520dce08818174ec2d69b5c40b12494c8e40
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2021-10-09 15:59:05 +0530

    Add files via upload

---
+++ hackankit/index.js
@@ -0,0 +1,25 @@
+let image = "HACTOMBER";
+let countel=document.getElementById("oops");
+
+function addi()
+{
+  image = image + "ankit";
+  countel.innerText=image
+}
+
+// function subtacti()
+// {
+//   image = image - "ankit";
+//   countel.innerText=image
+// }
+
+function save(){
+    console.log(image)
+
+}
+
+function reset()
+{
+  image = "HACTOMBER";
+  countel.innerText = "HACTOMBER";
+}
